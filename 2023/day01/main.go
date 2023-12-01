3ed32e52c9bc56255cbdbe49fdade4cbb54e7e37
Matthew Barrington <git@barrington.it>
2023-12-01 22:05:09 +0000

    Do day 1 a more sane way :D

--- 2023/day01/main.go
+++ 2023/day01/main.go
@@ -4,16 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"regexp"
+	"strings"
 )
 
-var lookup = map[string]int{
-	"one": 1, "two": 2, "three": 3,
-	"four": 4, "five": 5, "six": 6,
-	"seven": 7, "eight": 8, "nine": 9,
-}
+var (
+	lookupOne = map[string]int{
+		"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
+	}
 
-var foo = regexp.MustCompile(`twone|eightwo|oneight|eighthree|sevenine|one|two|three|four|five|six|seven|eight|nine|[0-9]`)
+	lookupTwo = map[string]int{
+		"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9,
+		"1": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9,
+	}
+
+	totals = map[int]int{1: 0, 2: 0}
+)
 
 func main() {
 	f, err := os.Open("input.txt")
@@ -22,41 +27,38 @@ func main() {
 	}
 
 	s := bufio.NewScanner(f)
-	s.Split(bufio.ScanLines)
 
-	var totals = map[int]int{1: 0, 2: 0}
 	for s.Scan() {
-		nums := map[int][]int{1: {}, 2: {}}
-		bar := foo.FindAll(s.Bytes(), -1)
-		for _, x := range bar {
-			if len(x) > 1 {
-				if string(x) == "twone" {
-					nums[2] = append(nums[2], 2, 1)
-				} else if string(x) == "oneight" {
-					nums[2] = append(nums[2], 1, 8)
-				} else if string(x) == "eighthree" {
-					nums[2] = append(nums[2], 8, 3)
-				} else if string(x) == "sevenine" {
-					nums[2] = append(nums[2], 7, 9)
-				} else if string(x) == "eightwo" {
-					nums[2] = append(nums[2], 8, 2)
-				} else {
-					nums[2] = append(nums[2], lookup[string(x)])
-				}
-			} else {
-				for k := range nums {
-					nums[k] = append(nums[k], (int(x[0] - 48)))
-				}
-			}
-		}
-		for k, v := range nums {
-			if len(v) == 1 {
-				totals[k] = totals[k] + v[0]*10 + v[0]
-			} else if len(v) > 1 {
-				totals[k] = totals[k] + v[0]*10 + v[len(v)-1]
-			}
-		}
+		s := s.Text()
+		totals[1] += find(s, lookupOne)
+		totals[2] += find(s, lookupTwo)
 	}
 	fmt.Println("part 1:", totals[1])
 	fmt.Println("part 2:", totals[2])
+}
+
+func find(s string, l map[string]int) int {
+	return findFirst(s, l)*10 + findLast(s, l)
+}
+
+func findFirst(s string, lookup map[string]int) int {
+	for {
+		for k, v := range lookup {
+			if strings.HasPrefix(s, k) {
+				return v
+			}
+		}
+		s = s[1:]
+	}
+}
+
+func findLast(s string, lookup map[string]int) int {
+	for {
+		for k, v := range lookup {
+			if strings.HasSuffix(s, k) {
+				return v
+			}
+		}
+		s = s[:len(s)-1]
+	}
 }
