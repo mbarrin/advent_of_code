a4e8bfffcf02e6f22cf7e414edc5ac69b1c45266
Matthew Barrington <git@barrington.it>
2023-12-05 11:43:35 +0000

    AoC 2023 Day 5

---
+++ 2023/day05/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+type mapping struct {
+	to   string
+	info []info
+}
+
+type info struct {
+	min, max int
+	offset   int
+}
+
+var transformations = make(map[string]*mapping)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		panic(1)
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+
+	lines := []string{}
+	for s.Scan() {
+		lines = append(lines, s.Text())
+	}
+
+	seeds := strings.Split(lines[0], " ")[1:]
+
+	var name, dest string
+
+	for i := 1; i < len(lines); i++ {
+		if strings.HasSuffix(lines[i], "map:") {
+			mappingLine := strings.Split(lines[i][:len(lines[i])-5], "-")
+			name, dest = mappingLine[0], mappingLine[2]
+
+			transformations[name] = &mapping{to: dest}
+
+		} else if lines[i] == "" {
+		} else {
+			var to, from, count int
+			fmt.Sscanf(lines[i], "%d %d %d", &to, &from, &count)
+			mappingInfo := info{min: from, max: from + count, offset: to - from}
+			transformations[name].info = append(transformations[name].info, mappingInfo)
+		}
+	}
+	totals := []int{}
+	for _, seed := range seeds {
+		i, _ := strconv.Atoi(seed)
+		totals = append(totals, search("seed", i))
+	}
+
+	fmt.Println("part 1:", slices.Min(totals))
+
+	totals = []int{}
+	for i := 0; i < len(seeds)-1; i += 2 {
+		foo, _ := strconv.Atoi(seeds[i])
+		bar, _ := strconv.Atoi(seeds[i+1])
+		for j := foo; j < foo+bar; j++ {
+			totals = append(totals, search("seed", j))
+		}
+	}
+
+	fmt.Println("part 2:", slices.Min(totals))
+}
+
+func search(name string, i int) int {
+	for _, x := range transformations[name].info {
+		if i >= x.min && i <= x.max {
+			i = i + x.offset
+			break
+		}
+	}
+	if transformations[name].to != "location" {
+		i = search(transformations[name].to, i)
+	}
+
+	return i
+}
