9124415fd114fe5a13c7c304bc7672745f0ca978
Matthew Barrington <git@barrington.it>
2023-12-06 11:17:39 +0000

    AoC 2023 Day 6 cleanup

--- 2023/day06/main.go
+++ 2023/day06/main.go
@@ -1,64 +1,46 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
 var r = regexp.MustCompile(`(\d+)`)
 
 func main() {
-	f, err := os.Open(os.Args[1])
+
+	f, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		panic(1)
 	}
 
-	s := bufio.NewScanner(f)
-
-	x := []string{}
-	y := []string{}
-	for s.Scan() {
-		a := strings.Join(r.FindAllString(s.Text(), -1), " ")
-		b := strings.Join(r.FindAllString(s.Text(), -1), "")
-		x = append(x, a)
-		y = append(y, b)
-	}
-
-	var one, two, three, four, five, six, seven, eight int
-	time := x[0]
-	fmt.Sscanf(time, "%d %d %d %d", &one, &two, &three, &four)
-	distance := x[1]
-	fmt.Sscanf(distance, "%d %d %d %d", &five, &six, &seven, &eight)
-
-	foo := map[int]int{
-		one:   five,
-		two:   six,
-		three: seven,
-		four:  eight,
+	nums := r.FindAllString(string(f), -1)
+	info := map[int]int{}
+	for i := 0; i < len(nums)/2; i++ {
+		key, _ := strconv.Atoi(nums[i])
+		val, _ := strconv.Atoi(nums[i+len(nums)/2])
+		info[key] = val
 	}
 
 	total := 1
-	for k, v := range foo {
+	for k, v := range info {
 		total *= succesful(k, v)
 	}
 
 	fmt.Println("part 1:", total)
 
-	time = y[0]
-	fmt.Sscanf(time, "%d", &one)
-	distance = y[1]
-	fmt.Sscanf(distance, "%d", &two)
-	fmt.Println("part 2:", succesful(one, two))
+	t, d := strings.Join(nums[:len(nums)/2], ""), strings.Join(nums[len(nums)/2:], "")
+
+	time, _ := strconv.Atoi(t)
+	dist, _ := strconv.Atoi(d)
+
+	fmt.Println("part 2:", succesful(time, dist))
 }
 
-func succesful(t, d int) int {
-	if t == 0 {
-		return 1
-	}
-	count := 0
+func succesful(t, d int) (count int) {
 	for i := 1; i < t; i++ {
 		if i*(t-i) > d {
 			count++
