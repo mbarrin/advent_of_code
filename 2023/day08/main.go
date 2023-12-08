c6f0c156aa46d2df0900718e76b3e07c511c42d1
Matthew Barrington <git@barrington.it>
2023-12-08 17:46:45 +0000

    Clean up 2023 Day 8

--- 2023/day08/main.go
+++ 2023/day08/main.go
@@ -15,11 +15,10 @@ type node struct {
 
 type data struct {
 	sync.RWMutex
+	sync.WaitGroup
 	cycles map[string]int
 }
 
-var c = make(chan int)
-var foo = make(chan int)
 var nums = []int{}
 
 func main() {
@@ -40,44 +39,33 @@ func main() {
 	nodes := make(map[string]node)
 	re := regexp.MustCompile(`(\w+)`)
 
-	starts, ends := []string{}, []string{}
+	starts := []string{}
 	for i := 2; i < len(lines); i++ {
 		parsed := re.FindAllString(lines[i], -1)
 		if parsed[0][2] == 'A' {
 			starts = append(starts, parsed[0])
-		} else if parsed[0][2] == 'Z' {
-			ends = append(ends, parsed[0])
 		}
-
 		nodes[parsed[0]] = node{left: parsed[1], right: parsed[2]}
 	}
 
-	wg := sync.WaitGroup{}
-
-	wg.Add(2)
-	go steps("AAA", "ZZZ", instructions, nodes, &wg)
-	go getVals(&wg)
-	wg.Wait()
-
-	fmt.Println("part 1:", nums[0])
-
-	blah := data{cycles: map[string]int{}}
+	d := data{cycles: map[string]int{}}
 	for _, start := range starts {
-		wg.Add(1)
-		go stepsNew(start, instructions, nodes, &wg, &blah)
+		d.Add(1)
+		go steps(start, instructions, nodes, &d)
 	}
-	wg.Wait()
+	d.Wait()
 
 	params := []int{}
-	for _, v := range blah.cycles {
+	for _, v := range d.cycles {
 		params = append(params, v)
 	}
+
+	fmt.Println("part 1:", d.cycles["AAA"])
 	fmt.Println("part 2:", LCM(params[0], params[1], params[1:]...))
 }
 
-func stepsNew(start string, instructions []string, nodes map[string]node, wg *sync.WaitGroup, b *data) {
+func steps(start string, instructions []string, nodes map[string]node, d *data) {
 	counter := 0
-
 	key := start
 	ok := true
 
@@ -90,43 +78,18 @@ func stepsNew(start string, instructions []string, nodes map[string]node, wg *sy
 			key = nodes[key].right
 		}
 
-		counter++
-
 		if strings.HasSuffix(key, "Z") {
 			ok = false
 		}
-	}
 
-	b.Lock()
-	b.cycles[start] = counter
-	b.Unlock()
-
-	wg.Done()
-}
-
-func steps(start, end string, instructions []string, nodes map[string]node, wg *sync.WaitGroup) {
-	counter := 0
-
-	key := start
-	for key != end {
-		i := instructions[counter%len(instructions)]
-		if i == "L" {
-			key = nodes[key].left
-		} else if i == "R" {
-			key = nodes[key].right
-		}
 		counter++
 	}
-	c <- counter
-	close(c)
-	wg.Done()
-}
 
-func getVals(wg *sync.WaitGroup) {
-	for y := range c {
-		nums = append(nums, y)
-	}
-	wg.Done()
+	d.Lock()
+	d.cycles[start] = counter
+	d.Unlock()
+
+	d.Done()
 }
 
 func GCD(a, b int) int {
